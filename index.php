87330a645c615feabd18678ac42e2bdb5d239b28
MadMaxInsight <maxvarvarin3@gmail.com>
2022-07-27 15:57:17 +0300

    Настроил htacess для вложенной сборки

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/* Попытка на беке скидывать кеш*/
+if(preg_match('#^www\.*#', strval($_SERVER['HTTP_HOST']))){
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+    header("Location: http://" . preg_replace('#^www\.*#', '',  $_SERVER['HTTP_HOST']));
+    include( 'build/index.html' );
+}elseif($_SERVER['REQUEST_URI'] == '/robots.txt'){
+    echo '<pre>';
+    include( 'build/robots.txt' );
+    echo '</pre>';
+}elseif($_SERVER['REQUEST_URI'] == '/sitemap.xml'){
+    header('Content-Type: application/xml');
+    $xml = file_get_contents('build/sitemap.xml');
+    echo $xml;
+}else{
+    include( 'build/index.html' );
+}
